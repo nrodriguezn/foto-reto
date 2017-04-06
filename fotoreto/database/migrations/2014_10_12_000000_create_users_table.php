72de3a9d6587a72287f46bc5091c5cf4eb566e71
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-04-06 09:52:04 -0500

    se arregla ; en migracion

--- fotoreto/database/migrations/2014_10_12_000000_create_users_table.php
+++ fotoreto/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('id_direction');
-            $table->integer('id_user_type')
+            $table->integer('id_user_type');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
