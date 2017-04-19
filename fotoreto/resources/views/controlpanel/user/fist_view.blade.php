64a8c2241b7af645da7fb2d440c89dc8137ede20
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-04-18 21:52:52 -0500

    algunos cambios en panel user

---
+++ fotoreto/resources/views/controlpanel/user/fist_view.blade.php
@@ -0,0 +1,65 @@
+<div class="container">
+  <h1>Panel de Usuario</h1>
+  <h2>Bienvenido {{ name }}</h2>
+  <section id="skill_sec">
+  	<div class="container">
+  		<div class="row">
+  			<div class="col-lg-12 col-md-12 col-sm-12 col-xs12 ">
+  				<div class="title_sec">
+  					<h1>Algunos graficos de la web</h1>
+  					<h2>Cantidad de usuarios, estado del fotoreto, inscritos esta semana, fotos subidas esta semana, etc...</h2>
+  				</div>
+  			</div>
+  		  <div class="skills progress-bar1">
+  				<ul class="col-md-6 col-sm-12 col-xs-12 wow fadeInLeft">
+  					  <li class="progress">
+  							<div class="progress-bar" data-width="85">
+  								  Wordpress 85%
+  							</div>
+  					  </li>
+  					  <li class="progress">
+  							<div class="progress-bar" data-width="65">
+  								  Graphic Design 65%
+  							</div>
+  					  </li>
+  					  <li class="progress">
+  							<div class="progress-bar" data-width="90">
+  								  HTML/CSS Design 90%
+  							</div>
+  					  </li>
+  					  <li class="progress">
+  							<div class="progress-bar" data-width="60">
+  								  SEO 60%
+  							</div>
+  					  </li>
+  				</ul>
+  				<ul class="col-md-6 col-sm-12 col-xs-12 wow fadeInRight">
+  					  <li class="progress">
+  							<div class="progress-bar" data-width="75">
+  								  Agencying 75%
+  							</div>
+  					  </li>
+  					  <li class="progress">
+  							<div class="progress-bar" data-width="95">
+  								  App Development 95%
+  							</div>
+  					  </li>
+  					  <li class="progress">
+  							<div class="progress-bar" data-width="70">
+  								  IT Consultency 70%
+  							</div>
+  					  </li>
+  					  <li class="progress">
+  							<div class="progress-bar" data-width="90">
+  								  Theme Development 90%
+  							</div>
+  					  </li>
+  				</ul>
+  		  </div>
+
+
+  		</div>
+  	</div>
+  </section>
+
+</div>
