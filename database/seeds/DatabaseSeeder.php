023e3f431e62cc1d778902bfa56c7fc6e460e8d4
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-10-29 14:11:42 -0500

    debo seguir revisando pero ya funciona

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Model::unguard();
+          $this->call(UserSeeder::class);
+        Model::reguard();
     }
 }
